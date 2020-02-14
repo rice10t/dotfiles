148dd69c48efea41dd95bfd2488337771b91c27b
nosoosso <nosoosso.null@gmail.com>
2020-02-15 03:56:39 +0900

    Refactor installation script

--- install.py
+++ install.py
@@ -2,22 +2,40 @@ import os
 from pathlib import Path
 import shutil
 
-if os.name == 'nt':
-    dotfiles = ['.ideavimrc']
-    for file in dotfiles:
+IDEAVIMRC = '.ideavimrc'
+TMUX_CONF = '.tmux.conf'
+INIT_VIM = 'init.vim'
+
+
+def is_windows():
+    return os.name == 'nt'
+
+
+def expands_to_home(files):
+    for file in files:
         shutil.copy2(file, str(Path.home()))
 
-    # Neovim
-    nvim_path = Path(os.environ['LOCALAPPDATA'], 'nvim')
-    nvim_path.mkdir(exist_ok=True)
-    shutil.copy2('init.vim', str(nvim_path))
 
-else:
-    dotfiles = ['.ideavimrc', '.tmux.conf']
-    for file in dotfiles:
-        shutil.copy2(file, str(Path.home()))
+def install(src, target):
+    target.mkdir(exist_ok=True)
+    shutil.copy2(src, str(target))
+
+
+def main():
+    if is_windows():
+        dotfiles = [IDEAVIMRC]
+        expands_to_home(dotfiles)
+    else:
+        dotfiles = [IDEAVIMRC, TMUX_CONF]
+        expands_to_home(dotfiles)
 
     # Neovim
-    nvim_path = Path.home() / '.config' / 'nvim'
-    nvim_path.mkdir(exist_ok=True)
-    shutil.copy2('init.vim', str(nvim_path))
+    if is_windows():
+        nvim_path = Path(os.environ['LOCALAPPDATA'], 'nvim')
+        install(INIT_VIM, nvim_path)
+    else:
+        nvim_path = Path.home() / '.config' / 'nvim'
+        install(INIT_VIM, nvim_path)
+
+
+main()
